9accf8ca04706145f99498bb8e3207ff358f56a1
tungning24 <tungning.p@gmail.com>
2026-06-03 20:06:43 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'walls-v1';
+
+const FILES = [
+  './',
+  './index.html',
+  './products.json'
+];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+
+  );
+
+});
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
